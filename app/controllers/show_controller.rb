f053c39150c407669a218eb165db046ae1401aa8
unknown <lenovo@.(none)>
2008-08-10 22:08:20 +0800

    add head

--- app/controllers/show_controller.rb
+++ app/controllers/show_controller.rb
@@ -3,6 +3,6 @@ class ShowController < ApplicationController
     @categories = Category.find(:all,:conditions=>"IsShow = 1",:order => "'Order' desc")
     #@c = Category.find(1);
     #@new_ents = Enterprise.find(:all, :conditions =>" ShowFirst =1 ",:order => "CreateTime desc")
-    @new_ents = Enterprise.find([223,2245,555,333,666,542,664])
+    @new_ents = Enterprise.find([223,2245,555,333,666,542,664,777,6643,44452])
   end
 end
